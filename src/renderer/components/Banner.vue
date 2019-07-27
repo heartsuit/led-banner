e292a953d31d54b572909279b489b3c665753fd6
heartsuit <nxq0108@126.com>
2019-07-27 15:50:03 +0800

    pass value from child to parent

--- src/renderer/components/Banner.vue
+++ src/renderer/components/Banner.vue
@@ -2,40 +2,64 @@
   <div id="banner">
     <header style="-webkit-app-region: drag">
       <section class="header">
-        <div class="content">{{content}}</div>
+        <div
+          :style="{fontFamily:properties.fontFamily, fontSize:properties.fontSize, 
+          color:properties.color, backgroundColor:properties.backgroundColor,
+          lineHeight:properties.lineHeight, letterSpacing:properties.letterSpacing}"
+        >{{content}}</div>
       </section>
+
       <section style="-webkit-app-region: no-drag">
         <!--html代码-->
       </section>
     </header>
+    <config @apply="update"></config>
   </div>
 </template>
 
 <script>
+import Config from "./Banner/Config";
+
 export default {
   name: "banner",
+  components: { Config },
   data() {
     return {
       content: "欢迎公益事业促进会会长一行莅临指导！",
+      properties: {
+        fontFamily: "",
+        fontSize: "",
+        color: "",
+        backgroundColor: "",
+        lineHeight: "",
+        letterSpacing: ""
+      }
     };
   },
   methods: {
-
+    update(params) {
+      this.properties = params;
+    }
   }
 };
 </script>
 
 <style>
-#banner {
-  color: yellow;
-  text-align: center;
-  background-color: red;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
-
+#banner {
+  text-align: center;
+}
+/* 
 .header .content {
   font-family: Microsoft YaHei;
   font-size: 80px;
+  color: yellow;
+  background-color: red;
   line-height: 100px;
   letter-spacing: 16px;
-}
+} */
 </style>
---
+++ src/renderer/components/Banner/Config.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div @click="apply">Config</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      properties: {
+        fontFamily: "Microsoft YaHei",
+        fontSize: "80px",
+        color: "yellow",
+        backgroundColor: "red",
+        lineHeight: "100px",
+        letterSpacing: "16px"
+      }
+    };
+  },
+  methods: {
+    apply() {
+      this.$emit("apply", this.properties);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
